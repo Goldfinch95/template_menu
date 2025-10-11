c70c421f2593f7b0e31db27da2f33b7e742a3e59
Goldfinch <facu_vila@hotmail.com>
2025-10-11 17:55:55 -0300

    la app esta para deployarse

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,8 @@
 export default function Home() {
   
   return (
-    <div>
-      
+    <div className="flex min-h-screen flex-col items-center justify-center p-24">
+      <span className="text-3xl font-bold">Listo, Jefe 🚀!</span>
     </div>
   );
 }
