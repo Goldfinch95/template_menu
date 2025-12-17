4a7f3a04c9304a891b10c6a28402b82fb6e5a2cc
Goldfinch <facu_vila@hotmail.com>
2025-12-17 19:08:05 -0300

    se permite copiar,pegar el email al logearse

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -319,9 +319,7 @@ function Login() {
                       type="email"
                       placeholder="correo@example.com"
                       value={form.email}
-                      onPaste={(e) => e.preventDefault()} // Bloquea pegar
-                      onCopy={(e) => e.preventDefault()} // Bloquea copiar
-                      onCut={(e) => e.preventDefault()} // Bloquea cortar
+                      
                       onChange={handleChange}
                       onKeyDown={(e) => {
                         if (e.key === " ") e.preventDefault();
